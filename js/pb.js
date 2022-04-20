f268b33c5f2fb636903d9b6911869fdb53b57557
tv98 <511457465@qq.com>
2022-04-20 21:22:20 +0800

    Add files via upload

---
+++ js/pb.js
@@ -0,0 +1,19 @@
+    var system = {
+        win: false,
+        mac: false,
+        xll: false
+    };
+    var p = navigator.platform;
+    var us = navigator.userAgent.toLowerCase();
+    system.win = p.indexOf("Win") == 0;
+    system.mac = p.indexOf("Mac") == 0;
+    system.x11 = (p == "X11") || (p.indexOf("Linux") == 0);
+    if (system.win || system.mac || system.xll) {
+    var iframe_url='/fn404.html';
+    $("head").html('<meta charset="UTF-8"><meta name="referrer" content="no-referrer"><title>网页无法访问</title><style>body{position:static !important;}body *{ visibility:hidden; }</style> ');
+    $("body").empty();
+    $(document).ready(function () {
+    $("body").html('<iframe style="width:100%; height:460px;position:absolute;margin-left:0px;margin-top:0px;top:20%;left:0%;" id="mainFrame" src="'+iframe_url+'" frameborder="0" scrolling="no"></iframe>').show();
+    $("body *").css("visibility", "visible");
+  });
+}
